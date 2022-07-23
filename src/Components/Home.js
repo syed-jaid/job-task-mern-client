feae664ea7fb18c24c789970fbbd58f933654ed3
md-adnan-ahsan <syed.jaid.7748@gmail.com>
2022-07-23 21:14:52 +0600

    work done

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import Login from './Login';
+import { Link } from 'react-router-dom';
 const Home = () => {
     return (
         <div className=' mt-8'>
-            <Login></Login>
+            <div class="hero min-h-screen bg-base-200">
+                <div class="hero-content text-center">
+                    <div class="max-w-md">
+                        <h1 class="text-5xl font-bold">Inventory Management System</h1>
+                        <p class="py-6">An inventory management system combines varying software packages to track stock levels and stock movements. The solution can integrate with multichannel sales systems or shipping systems.</p>
+                        <button class="btn btn-primary"><Link to='/login'></Link> Get Started</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
